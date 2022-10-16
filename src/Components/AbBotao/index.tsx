c0bc315943b4f5dbc5cf6feae8362a5d2af88e9a
Vanessa Silva - OTK <vanessa.silva@otk.com.br>
2022-10-16 20:38:02 -0300

    First commit

---
+++ src/Components/AbBotao/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import * as C from './style'
+
+export const AbBotao = () => {
+    return (
+        <C.Btn>
+            Clique aqui!
+        </C.Btn>
+    )
+}
---
+++ src/Components/AbBotao/style.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Btn = styled.button`
+    background-color: #EB9B00;
+    padding: 16px 32px;
+    border: 2px solid #EB9B00;
+    color: #F4F4F4;
+    font-size: 20px;
+    cursor: pointer;
+
+    &:hover{
+        background-color: #B87900;
+        border: 2px solid #B87900;
+    }
+`
