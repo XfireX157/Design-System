14e5f6bb6b63b4d4c875f99664577a7049c84a89
Gustavo <gustavopereirafacal@gmail.com>
2022-10-18 17:26:45 -0300

    saved Buttons Add

---
+++ stories/AbButtonQtd.stories.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import {AbButtonQtd} from '../src/Components/AbButtonQtd'
+import { IButtonQtds } from "../src/Components/AbButtonQtd/Types/IButtonsQtd";
+
+export default {
+    title: 'Components/AbButtonQtd',
+    component: AbButtonQtd
+} as ComponentMeta <typeof AbButtonQtd>
+
+const Template: ComponentStory<typeof AbButtonQtd> = (args: IButtonQtds) => <AbButtonQtd {...args} />
+
+export const Primary = Template.bind({})
+
+Primary.args = {
+    BtnLarger: "+",
+    BtnSmaller: "-",
+} as IButtonQtds
